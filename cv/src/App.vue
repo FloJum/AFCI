b4454fd685debe96dcaf80cc123e5d0300014288
Florian Jumeaucourt <fjumeaucourt@gmail.com>
2022-02-18 12:32:25 +0100

    hamburger réparé

--- cv/src/App.vue
+++ cv/src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <div id="nav">
     <nav
-      class="navbar fixed-top navbar-expand-lg navbar-light text-uppercase "
+      class="navbar fixed-top navbar-expand-lg container-fluid text-uppercase"
     >
       <button
         id="navbtn"
-        class="navbar-toggler custom-toggler ml-2"
+        class="navbar-toggler ml-2 custom-toggler"
         type="button"
         data-bs-toggle="collapse"
         data-bs-target="#navbarSupportedContent"
@@ -28,27 +28,27 @@
         class="collapse navbar-collapse ml-lg-3 justify-content-center"
         id="navbarSupportedContent"
       >
-        <div class="navbar-nav ">
-            <router-link to="/" class="nav-item nav-link" href="#ancre1"
-              >A propos de moi</router-link
-            >
-            <router-link to="/cv" class="nav-item nav-link">Mon CV</router-link>
-            <router-link to="/sites" class="nav-item nav-link" 
-              >Mes sites (à venir)</router-link
-            >
+        <div class="navbar-nav">
+          <router-link to="/" class="nav-item nav-link"
+            >A propos de moi</router-link
+          >
+          <router-link to="/cv" class="nav-item nav-link">Mon CV</router-link>
+          <router-link to="/sites" class="nav-item nav-link"
+            >Mes sites (à venir)</router-link
+          >
         </div>
       </div>
     </nav>
   </div>
   <router-view />
   <footer>
-      <h6>
-        Florian Jumeaucourt - Développeur Web - Aisne
-        <a href="mailto:fjumeaucourt@wolfsnake.website"
-          >fjumeaucourt@wolfsnake.website</a
-        >
-      </h6>
-    </footer>
+    <h6>
+      Florian Jumeaucourt - Développeur Web - Aisne
+      <a href="mailto:fjumeaucourt@wolfsnake.website"
+        >fjumeaucourt@wolfsnake.website</a
+      >
+    </h6>
+  </footer>
 </template>
 
 <style>

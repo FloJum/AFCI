e38e8d56651d033984c61483d4e5ee97331fb391
Florian Jumeaucourt <fjumeaucourt@gmail.com>
2022-02-22 16:26:12 +0100

    exophp

--- test.php
+++ test.php
@@ -5,5 +5,9 @@ for(reset($equipes); $key= key($equipes); next($equipes)){
     echo"$classement est en $key. <br>"; 
 }
 
-
+// Exemple 1
+$pizza  = "piece1 piece2 piece3 piece4 piece5 piece6";
+$pieces = explode(" ", $pizza);
+echo $pieces[0]; // piece1
+echo $pieces[1]; // piece2
 ?>
